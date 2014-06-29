5e425f67cec858cf9011005b9a88fbdd539666c8
calebx <caleb.xiang@gmail.com>
2014-06-29 23:48:35 +0800

    little

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
 set :environment, :development
-job_type :runner,  "cd :path && bin/rails runner -e :environment :task :output"
+set :output, "log/schedule.log"
+
+job_type :runner,  "cd :path && bin/rails runner -e :environment :task >> :output"
+job_type :command, ":task >> :output"
 
 every 5.minutes do
   command "echo 'hola 5 mins testing'"
