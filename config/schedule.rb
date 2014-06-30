c48452dbb3b2ce03544d26b62b13c4b5562a5d2b
calebx <caleb.xiang@gmail.com>
2014-06-30 10:37:18 +0800

    status check remove

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,6 @@ set :output, "log/schedule.log"
 job_type :runner,  "cd :path && bin/rails runner -e :environment :task :output"
 job_type :command, ":task :output"
 
-every 5.minutes do
-  command "echo 'hola 5 mins testing'"
-end
-
 every :hour do 
   runner "Work.start"
 end
